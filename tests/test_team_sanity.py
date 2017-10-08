8c2e378005e8458ce8cc71f5ed5a919f98971d87
AdrienMorvan <adrien.morvan@photobox.com>
2017-10-08 18:48:13 +0200

    Added a team sanity check

---
+++ tests/test_team_sanity.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function)
+
+from unittest import TestCase, skip
+import random
+import sys
+sys.path.append('../..')
+from industrie_futur.team_handling.team_handler import Team
+from redis import StrictRedis
+from config import Config as config
+import pickle
+import json
+
+
+class TestTeamRedis(TestCase):
+
+    __name__ = "smartbook_test"
+
+    def setUp(self):
+        self.r = StrictRedis()
+
+    def test_find_empty_teams(self):
+        teams = pickle.loads(self.r.get('teams'))
+        r.set('old_teams', self.r.get('teams'))
+        teams_to_delete = []
+
+        for t1 in teams:
+            if t1.name is None:
+                for t2 in teams:
+                    if t1.creator == t2.creator:
+                        print("Found empty duplicate team")
+                        teams_to_delete.append(t1)
+
+        print("Found {}".format([t.__dict__ for t in teams_to_delete]))
+
+
+
+
+
