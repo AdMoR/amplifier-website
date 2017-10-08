10a149a4a66134f126f7fef269a91ec250d61514
AdrienMorvan <adrien.morvan@photobox.com>
2017-10-08 18:57:20 +0200

    removed bug for the test for team

--- tests/test_team_sanity.py
+++ tests/test_team_sanity.py
@@ -28,12 +28,12 @@ class TestTeamRedis(TestCase):
         for t1 in teams:
             if t1.name is None:
                 for t2 in teams:
-                    if t1.creator == t2.creator:
+                    if t1.creator == t2.creator and t1.description is None:
                         print("Found empty duplicate team")
                         teams_to_delete.append(t1)
 
         for t in teams_to_delete:
-            teams.delete(t)
+            teams.remove(t)
         self.r.set('teams', pickle.dumps(teams))
 
         print("Found {}".format([t.__dict__ for t in teams_to_delete]))
