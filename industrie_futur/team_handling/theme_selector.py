1cc1ef3220294fcac6f6cd4ba400976569a678c2
AdrienMorvan <adrien.morvan@photobox.com>
2017-10-08 12:36:40 +0200

    Removed debug prints

--- industrie_futur/team_handling/theme_selector.py
+++ industrie_futur/team_handling/theme_selector.py
@@ -13,41 +13,6 @@ class ThemeSelector(object):
         '''
         self.preferences = preference_dicts
 
-    def find_admissible_repartition(self):
-        """
-        """
-
-        # 1 : give everyne what they want
-        repartitions = {t: self.preferences[t] for t in self.preferences.keys()
-                        if t in self.themes}
-        nb_per_theme = {t: len(repartitions[t]) for t in self.themes}
-        print(nb_per_theme)
-
-        # 2 : Assign the undecided to the needed themes
-        no_pref_themes = [key for key in self.preferences
-                          if key not in self.themes]
-        print(no_pref_themes)
-        for nopref in no_pref_themes:
-            if nopref not in self.preferences.keys():
-                continue
-
-            while len(self.preferences[nopref]) > 0:
-                smallest_theme = sorted(self.themes, key=lambda t: nb_per_theme[t])
-                user_to_add = self.preferences[nopref].pop()
-                repartitions[smallest_theme[0]].append(user_to_add)
-                nb_per_theme = {t: len(repartitions[t]) for t in self.themes}
-
-        # 3 : Assign according to second choice in
-        smallest_theme = sorted(self.themes, key=lambda t: nb_per_theme[t])
-        biggest_theme = sorted(self.themes, key=lambda t: -nb_per_theme[t])
-        ratio = float(len(repartitions[smallest_theme[0]])) / float(len(repartitions[biggest_theme[0]]))
-        #while ratio > 1.3:
-        #    for user in repartitions[smallest_theme[0]]:
-        #        if user in second_preferences.keys():
-
-        print(nb_per_theme)
-        return repartitions
-
     def assign_theme_to_users(self, themes=['t1', 't2', 't3'], filler='t0'):
         """
         """
@@ -90,7 +55,6 @@ class ThemeSelector(object):
         max_difference = one_step(repartitions, themes)
         # Do the random assignement while the gap is too big
         while max_difference > filler_margin:
-            print(max_difference)
             max_difference = one_step(repartitions, themes)
 
         # Assign randomly the filler
@@ -108,16 +72,16 @@ class ThemeSelector(object):
     def brute_force_filling(self, repartitions, themes, filler, filler_margin=10):
         all_solutions = self.generate_solution_space(users=self.preferences.keys(),
                                                      themes=themes)
-        print('all_solutions', all_solutions)
         best_solution = None
         best_score = 100000
 
         for solution in all_solutions:
-            print(solution)
             score = self.evaluate_solution(solution, self.preferences)
             if score < best_score:
                 best_solution = solution
                 best_score = score
+
+        print(best_solution)
 
         return best_solution, best_score
 
@@ -129,7 +93,6 @@ class ThemeSelector(object):
 
 
     def generate_solution_space(self, users, themes, limit=10000):
-        print('users', users)
         index_to_user = {i: user for i, user in enumerate(users)}
         all_permutations = itertools.permutations(range(len(users)))
 
