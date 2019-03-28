0af41867cba01a22232f0683f5833faedf818e44
Admor <adrien_morvan@hotmail.fr>
2019-03-28 12:03:22 +0100

    Add music mixing

--- amplifier/controller.py
+++ amplifier/controller.py
@@ -5,6 +5,7 @@ import json
 from flask import render_template, Blueprint, jsonify, request, send_from_directory, g
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.wrappers import Response
+from pydub import AudioSegment
 import random
 from config import Config as config
 from . import LOG, sentry, login_manager
@@ -44,14 +45,37 @@ def form():
 @api_v1.route("/form", methods=['POST'])
 def post_form():
     sent_back_form = request.form
+    name = sent_back_form.get("name")
     ad_text = sent_back_form.get('ad_text')
+    background_type = sent_back_form.get('music')
+
+    print(background_type)
+    sound_path = combine_audios(audio_a="amplifier/static/sound/test.wav", audio_b="amplifier/static/sound/{}.wav".format(background_type), export_name=name)
+
     print(">>>>>>> ", ad_text)
-    return render_template("music_display.html", caption=ad_text), 200
+    sound_path = "sound/wurth1_with_music.wav"
+    return render_template("music_display.html", caption=ad_text, audio_file=sound_path), 200
 
 
 @api_v1.route("/music", methods=['GET'])
 def music():
-    return render_template("music_display.html"), 200
+    sound_path = "sound/wurth1_with_music.wav"
+    return render_template("music_display.html", audio_file=sound_path), 200
+
+
+######### Helpers ###################################
+
+
+def combine_audios(audio_a="test.wav", audio_b="ModernFashion.wav", export_name="export"):
+
+    export_path = "sound/{}.wav".format(export_name)
+
+    voice1 = AudioSegment.from_file(audio_a)
+    soundtrack = AudioSegment.from_file(audio_b)
+    combined = soundtrack.overlay(voice1, position=5000)
+
+    combined.export("amplifier/static/" + export_path, format='wav')
+    return export_path
 
 
 
